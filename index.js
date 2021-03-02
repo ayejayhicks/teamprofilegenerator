83cf8a87c3334032f6c924c20e3d4e686a400d0e
Alaina Hicks <ajhicks0816@gmail.com>
2021-03-02 00:32:13 -0600

    unsure

--- index.js
+++ index.js
@@ -38,9 +38,7 @@ background-color: rgb(6, 73, 161);
 </style>
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
-  <div class= text-align="center"></div>
     <h1 class="display-4">My Team</h1>
-    
   </div>
 </div>
 
@@ -70,7 +68,7 @@ background-color: rgb(6, 73, 161);
         <p class="card-text"></p>
         <p class="card-text"></p>
         <p class="card-text"></p>
-    </div>  
+    </div>
 </div>
 </div>
 <div class="row">
