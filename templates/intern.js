d27d1db109a960ba3a4bb2fb6f9b6684a3acf208
Alaina Hicks <ajhicks0816@gmail.com>
2021-05-21 02:10:37 -0500

    syntax error fix lib files, cards, index/app

--- templates/intern.js
+++ templates/intern.js
@@ -1,21 +1,30 @@
-function intern(intern) {
-    return `
-    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-    <div class="card-header"></div>
-    <div class="card-header"></div>
-    <div class="card-body">
-            <div class="card-header">
-        <h3>${intern.name}</h3>
-        <h4>Intern</h4>
-        <h6>${intern.ID}</h6>
-    </div>
-    <div class="bg-trans card-body">
-        <h6 class="card-title">${intern.email}</h6>
-        <h6 class="card-title">${intern.school}</h6>
-    </div>
-    </div>
-    </div>
-    </div>`
-    };
+<div class="card mx-auto" style="width: 18rem">
+    <h5 class="card-header">Cannon Hicks<br /><br />Intern</h5>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID</li>
+        <li class="list-group-item">email</li>
+        <li class="list-group-item">School</li>
+    </ul>
+</div>
+
+// function intern(intern) {
+//     return `
+//     <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+//     <div class="card-header"></div>
+//     <div class="card-header"></div>
+//     <div class="card-body">
+//             <div class="card-header">
+//         <h3>${intern.name}</h3>
+//         <h4>Intern</h4>
+//         <h6>${intern.id}</h6>
+//     </div>
+//     <div class="bg-trans card-body">
+//         <h6 class="card-title">${intern.email}</h6>
+//         <h6 class="card-title">${intern.school}</h6>
+//     </div>
+//     </div>
+//     </div>
+//     </div>`
+//     };
     
-    module.exports = intern;
+//     module.exports = intern;
--- templates/manager.js
+++ templates/manager.js
@@ -1,21 +1,30 @@
-function managerInfo(answer) {
-    return `
-    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-    <div class="card-header"></div>
-    <div class="card-header"></div>
-    <div class="card-body">
-        <div class="card-header">
-        <h3>${answer.name}</h3>
-        <h4>Manager</h4>
-        <h6>${answer.id}</h6>
-    </div>
-    <div class="bg-trans card-body">
-        <h6 class="card-title">${answer.email}</h6>
-        <h6 class="card-title">${answer.officeNumber}</h6>
-    </div>
-    </div>
-    </div>
-    </div>`
-    };
+<div class="card mx-auto" style="width: 18rem">
+    <h5 class="card-header">Grae Hicks<br /><br />Manager</h5>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID</li>
+        <li class="list-group-item">email</li>
+        <li class="list-group-item">Office Phone</li>
+    </ul>
+</div>
 
-    module.exports = managerInfo;
+// function managerInfo(answer) {
+//     return `
+//     <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+//     <div class="card-header"></div>
+//     <div class="card-header"></div>
+//     <div class="card-body">
+//         <div class="card-header">
+//         <h3>${answer.name}</h3>
+//         <h4>Manager</h4>
+//         <h6>${answer.id}</h6>
+//     </div>
+//     <div class="bg-trans card-body">
+//         <h6 class="card-title">${answer.email}</h6>
+//         <h6 class="card-title">${answer.officeNumber}</h6>
+//     </div>
+//     </div>
+//     </div>
+//     </div>`
+//     };
+
+//     module.exports = managerInfo;
